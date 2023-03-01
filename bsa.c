6fda7c2cdb2dd0a2e050281dde83c04561c4f987
Dan Sanderson <contact@dansanderson.com>
2023-02-28 20:42:26 -0800

    Repair MACRO MACRO has a bug where RecordMacro reads to the end of the file in the final pass when attempting to read the definition into the lst file, (because the macro definition has already been skipped in the previous loop), preventing code generation after the first macro definition. This repair combines the "skip definition" and "print definition to lst" parts of the routine that execute during the final pass.

--- bsa.c
+++ bsa.c
@@ -3428,27 +3428,24 @@ void RecordMacro(char *p)
          }
          Macros++;
       }
-      else     // skip macro body
+      else    // skip macro body, printing to lf on last pass
       {
-         while (!feof(sf) && !Strcasestr(Line,"ENDMAC"))
-         {
-            ++LiNo;
-            fgets(Line,sizeof(Line),sf);
-         }
-      }
-      if (Pass == MaxPass) // List macro
-      {
+         if (Pass == MaxPass) {
          PrintLiNo(1);
          ++LiNo;
          fprintf(lf,"            %s\n",Line);
-         do
+         }
+         while (!feof(sf) && !Strcasestr(Line,"ENDMAC"))
          {
+         ++LiNo;
             fgets(Line,sizeof(Line),sf);
+            if (Pass == MaxPass) {
             PrintLiNo(1);
             ++LiNo;
             fprintf(lf,"            %s",Line);
             if (pf) fprintf(pf,"%s",Line);
-         } while (!feof(sf) && !Strcasestr(Line,"ENDMAC"));
+            }
+         }
          LiNo-=2;
       }
       if (df) fprintf(df,"Macro [%s] = %s\n",Mac[j].Name,Mac[j].Body);
